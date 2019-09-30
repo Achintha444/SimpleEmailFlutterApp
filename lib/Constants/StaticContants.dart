3953eb19593adc66ba66c96d165ec2b9615f420d
Achinths Isuru <achinthaisuru444@gmail.com>
2019-09-30 23:44:10 +0530

    created the first interface

---
+++ lib/Constants/StaticContants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class StaticContants{
+  static final String imagesLocation = "assets/images/";
+  static final Color mainColor = Color.fromRGBO(62,101,169, 1);
+  static final Color secondaryLighterColor = Color.fromRGBO(158,178,212, 1);
+  static final Color secondaryDarkerColor = Color.fromRGBO(31,50,84, 1);
+  static final Color backgroundColor = Colors.white;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,111 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+import 'Constants/StaticContants.dart';
+
+void main() => runApp(
+      new MaterialApp(
+        home: FirstInterface(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class FirstInterface extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+    return new Scaffold(
+      body: new Container(
+        //padding: EdgeInsets.only(top: _height / 18),
+        color: StaticContants.backgroundColor,
+        alignment: Alignment.center,
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            new Image(
+              image: AssetImage("assets/images/emailIcon.jpg"),
+              height: _height / 3,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new Container(
+              padding: EdgeInsets.only(top: _height / 30),
+              child: new Text(
+                "Fast EMAIL",
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                    color: StaticContants.mainColor,
+                    fontSize: _height / 10,
+                    letterSpacing: 2,
+                    wordSpacing: 3,
+                    fontWeight: FontWeight.w900,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+            new Container(
+              padding: EdgeInsets.only(
+                top: _height / 30,
+                bottom: _height / 30,
+              ),
+              child: new Text(
+                "~ Email Sending APP ~",
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  color: StaticContants.mainColor,
+                  fontSize: _height / 30,
+                  letterSpacing: 1.5,
+                  wordSpacing: 2,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            new Container(
+              padding: EdgeInsets.only(
+                left: _width / 10,
+                right: _width / 10,
+              ),
+              child: new RaisedButton(
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Image(
+                      image: AssetImage(
+                          StaticContants.imagesLocation + "googleLogo.jpg"),
+                      height: _height / 15,
+                    ),
+                    new Container(
+                      padding: EdgeInsets.only(left: _width / 25),
+                      child: new Text(
+                        "LOGIN",
+                        style: new TextStyle(
+                          color: StaticContants.backgroundColor,
+                          fontSize: _height / 30,
+                          letterSpacing: 1.5,
+                          wordSpacing: 2,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                shape: new RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                onPressed: () {},
+                color: StaticContants.mainColor,
+                padding: EdgeInsets.only(
+                  top: _height / 80,
+                  bottom: _height / 80,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
